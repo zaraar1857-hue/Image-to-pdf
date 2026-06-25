7f84a03b8e6eed08d1399cd85e8cd9215248fd8e
zaraar1857-hue <zaraar1857@gmail.com>
2026-06-25 11:25:00 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
+// Monetag Configuration
+self.options = { 
+    "domain": "3nbf4.com", 
+    "zoneId": 11198564 
+}; 
+self.lary = ""; 
+
+// Importing the Monetag service worker script
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
+
+// Service worker fetch event listener
 self.addEventListener('fetch', () => {});
